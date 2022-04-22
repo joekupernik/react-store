a79c4fd5866a163d3c05a4da6421303c6e98c73d
Joe Kupernik <joekupernik@gmail.com>
2022-04-22 11:21:16 -0700

    cart to right side of nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,31 +1,27 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav";
 import {Link } from "react-router-dom";
-import Badge from "react-bootstrap/Badge";
-import Button from "react-bootstrap/Button";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
 
 
 
 function Header (){
     return (
        <>
-    <Nav variant="tabs" defaultActiveKey="/home">
-  <Nav.Item>
-    <Nav.Link eventKey="/"> <Link to="/">Home</Link></Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="/Account"> <Link to="/Account">Account</Link></Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="/Cart"><Link to="/Cart">
-    <Button variant="primary">
-  Cart <Badge bg="secondary">9</Badge>
-</Button></Link></Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="/Login"><Link to="/Login">Login</Link></Nav.Link>
-  </Nav.Item>
-</Nav>
+  <Navbar bg="dark"  variant="dark" sticky="top">
+    <Container>
+    <Navbar.Brand >Awesome Store!!</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link ><Link to="/">Home</Link></Nav.Link>
+    </Nav>
+    <Nav className="justify-content-end">
+    <Nav.Link ><Link to="/Login">Login</Link></Nav.Link>
+    <Nav.Link > <Link to="/Cart"> Cart</Link></Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
        </>
     );
 }
