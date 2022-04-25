7240aeb66bd85f56dc919ac2fb8d2fd613261724
Joe Kupernik <joekupernik@gmail.com>
2022-04-25 11:24:41 -0700

    basic login form

--- src/routes/LogIn.js
+++ src/routes/LogIn.js
@@ -1,18 +1,60 @@
 import React from "react";
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+
+
+
+
 
 
 function Account() {
-    return (
-      <>
-        <main>
-          <h2>Who are we?</h2>
-          <p>
-            That feels like an existential question, don't you
-            think?
-          </p>
-        </main>
-      </>
-    );
-  }
+  return (
+    <>
+      <main>
+        <h1>Log In</h1>
+        <Form>
+          <Row className="align-items-center">
+            <Col xs="auto">
+              <Form.Label htmlFor="inlineFormInput" visuallyHidden>
+                Name
+              </Form.Label>
+              <Form.Control
+                className="mb-2"
+                id="inlineFormInput"
+                placeholder="Jane Doe"
+              />
+            </Col>
+            <Col xs="auto">
+              <Form.Label htmlFor="inlineFormInputGroup" visuallyHidden>
+                Username
+              </Form.Label>
+              <InputGroup className="mb-2">
+                <InputGroup.Text>@</InputGroup.Text>
+                <FormControl id="inlineFormInputGroup" placeholder="Username" />
+              </InputGroup>
+            </Col>
+            <Col xs="auto">
+              <Form.Check
+                type="checkbox"
+                id="autoSizingCheck"
+                className="mb-2"
+                label="Remember me"
+              />
+            </Col>
+            <Col xs="auto">
+              <Button type="submit" className="mb-2">
+                Submit
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </main>
+    </>
+  );
+}
 
-  export default Account;
+export default Account;
