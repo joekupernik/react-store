ceab3da60751a0263cf10fcac393c537a4ef5dca
Joe Kupernik <joekupernik@gmail.com>
2022-04-28 14:56:44 -0700

    checkout form basic

---
+++ src/components/CheckoutForm/CheckoutForm.js
@@ -0,0 +1,105 @@
+import React, {useState} from "react";
+//import { Formik, Form, } from 'formik';
+//import yup from 'yup';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form'
+
+
+
+
+
+function CheckoutForm() {
+
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        setValidated(true);
+    };
+
+
+    return (
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Row className="mb-3">
+                <Form.Group as={Col} md="4" controlId="validationCustom01">
+                    <Form.Label>First name</Form.Label>
+                    <Form.Control
+                        required
+                        type="text"
+                        placeholder="First name"
+                        defaultValue="Mark"
+                    />
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group as={Col} md="4" controlId="validationCustom02">
+                    <Form.Label>Last name</Form.Label>
+                    <Form.Control
+                        required
+                        type="text"
+                        placeholder="Last name"
+                        defaultValue="Otto"
+                    />
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+                    <Form.Label>Adress</Form.Label>
+                    <InputGroup hasValidation>
+                        <Form.Control
+                            type="text"
+                            placeholder="Adress"
+                            aria-describedby="inputGroupPrepend"
+                            required
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Please enter a valid address.
+                        </Form.Control.Feedback>
+                    </InputGroup>
+                </Form.Group>
+            </Row>
+            <Row className="mb-3">
+                <Form.Group as={Col} md="6" controlId="validationCustom03">
+                    <Form.Label>City</Form.Label>
+                    <Form.Control type="text" placeholder="City" required />
+                    <Form.Control.Feedback type="invalid">
+                        Please provide a valid city.
+                    </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group as={Col} md="3" controlId="validationCustom04">
+                    <Form.Label>State</Form.Label>
+                    <Form.Control type="text" placeholder="State" required />
+                    <Form.Control.Feedback type="invalid">
+                        Please provide a valid state.
+                    </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group as={Col} md="3" controlId="validationCustom05">
+                    <Form.Label>Zip</Form.Label>
+                    <Form.Control type="text" placeholder="Zip" required />
+                    <Form.Control.Feedback type="invalid">
+                        Please provide a valid zip.
+                    </Form.Control.Feedback>
+                </Form.Group>
+            </Row>
+            <Form.Group className="mb-3">
+                <Form.Check
+                    required
+                    label="Agree to terms and conditions"
+                    feedback="You must agree before submitting."
+                    feedbackType="invalid"
+                />
+            </Form.Group>
+            <Button type="submit">Submit form</Button>
+        </Form>
+    );
+}
+
+
+
+export default CheckoutForm;
--- src/routes/Checkout.js
+++ src/routes/Checkout.js
@@ -1,8 +1,12 @@
 import React from "react";
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
+//import Form from 'react-bootstrap/Form';
+//import Row from 'react-bootstrap/Row';
+//import Col from 'react-bootstrap/Col';
+//import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+import CheckoutForm from '../components/CheckoutForm/CheckoutForm';
+
 
 function Account() {
   return (
@@ -11,130 +15,14 @@ function Account() {
         <h1>Checkout</h1>
         <div style={{ padding: '2%', margin: '3%' }}>
         <h2>Billing address</h2>
-          <Form>
-            <Row className="mb-3">
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>First Name</Form.Label>
-                <Form.Control type="text" placeholder="First Name" />
-              </Form.Group>
+          <Card>
+            <CheckoutForm />
+          </Card>
+        <h2>Shipping address</h2>
+        
 
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control type="text" placeholder="Last Name" />
-              </Form.Group>
-            </Row>
-
-            <Row className="mb-3">
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Label>Phone Number</Form.Label>
-                <Form.Control type="number" placeholder="Phone Number" />
-              </Form.Group>
-            </Row>
-
-            <Form.Group className="mb-3" controlId="formGridAddress1">
-              <Form.Label>Address</Form.Label>
-              <Form.Control placeholder="1234 Main St" />
-            </Form.Group>
-
-            <Row className="mb-3">
-              <Form.Group as={Col} controlId="formGridCity">
-                <Form.Label>City</Form.Label>
-                <Form.Control />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridState">
-                <Form.Label>State</Form.Label>
-                <Form.Select defaultValue="Choose...">
-                  <option>Choose...</option>
-                  <option>...</option>
-                </Form.Select>
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridZip">
-                <Form.Label>Zip</Form.Label>
-                <Form.Control />
-              </Form.Group>
-            </Row>
-            <Form.Group className="mb-3" id="formGridCheckbox">
-              <Form.Check type="checkbox" label="Shipping adress is the same as Billing" />
-            </Form.Group>
-
-            <h2>Shipping address</h2>
-            <Row className="mb-3">
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>First Name</Form.Label>
-                <Form.Control type="text" placeholder="First Name" />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control type="text" placeholder="Last Name" />
-              </Form.Group>
-            </Row>
-
-            <Form.Group className="mb-3" controlId="formGridAddress1">
-              <Form.Label>Address</Form.Label>
-              <Form.Control placeholder="1234 Main St" />
-            </Form.Group>
-
-            <Row className="mb-3">
-              <Form.Group as={Col} controlId="formGridCity">
-                <Form.Label>City</Form.Label>
-                <Form.Control />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridState">
-                <Form.Label>State</Form.Label>
-                <Form.Select defaultValue="Choose...">
-                  <option>Choose...</option>
-                  <option>...</option>
-                </Form.Select>
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridZip">
-                <Form.Label>Zip</Form.Label>
-                <Form.Control />
-              </Form.Group>
-            </Row>
-
-            <h2>Payment</h2>
-            <Row className="mb-3">
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>Name On Card</Form.Label>
-                <Form.Control type="text" placeholder="Full name as displayed on card" />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Label>Credit Card Number</Form.Label>
-                <Form.Control type="integer" placeholder="Credit Card" />
-              </Form.Group>
-            </Row>
-            <Row className="mb-3">
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>Experation</Form.Label>
-                <Form.Control type="date" />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Label>CVV</Form.Label>
-                <Form.Control type="integer" />
-              </Form.Group>
-            </Row>
-
+        <h2>Payment</h2>
             
-
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-            
-
-          </Form>
-
         </div>
       </main>
 
