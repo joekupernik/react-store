d1b40e47d74aad456d3b5433b617baf94c191ec6
Joe Kupernik <joekupernik@gmail.com>
2022-04-27 18:07:41 -0700

    product card w fake backend done

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -2,7 +2,6 @@
 import React, { useEffect, useState } from "react";
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-//import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
