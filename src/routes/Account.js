6bc0e940bc3babdec8e12a43572c7f3c690b2148
Joe Kupernik <joekupernik@gmail.com>
2022-04-29 14:50:27 -0700

    d

--- src/routes/Account.js
+++ src/routes/Account.js
@@ -1,22 +1,32 @@
 import React from "react";
-import {Link } from "react-router-dom";
+//import {Link } from "react-router-dom";
+//import Col from 'Col';
+//import Row from 'Row';
+//import Card from 'react-bootstrap/Card';
+//import Button from 'react-bootstrap/Button';
+
 
 
 function Account() {
     return (
       <>
         <main>
-          <h1>Welcome John</h1>
+
+            <h1>Welcome John</h1>
+            
           <hr
             style={{
               color: "black",
               height: 3
             }}
           />
+          <h2>Your Information</h2>
+          <p>name</p>
+          <p>name</p>
+          <p>name</p>
+          <p>name</p>
+     
         </main>
-        <div>
-          
-        </div>
       </>
     );
   }
