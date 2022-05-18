5dcb465ead24703b1f2fb001aa1a65bd45a224e9
Joe Kupernik <joekupernik@gmail.com>
2022-05-18 13:18:28 -0700

    cart button css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,9 +3,9 @@ import Nav from "react-bootstrap/Nav";
 import {Link } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import Button from "react-bootstrap/Button";
 import Badge from 'react-bootstrap/Badge';
 import Logo from '../../asset/Logo.png';
+import Cart from '../../asset/Cart.png';
 
 
 
@@ -17,22 +17,23 @@ function Header (){
     <Container>
     <Navbar.Brand>
    <Link to="/">
-    <img src={Logo} alt="logo" style={{ height: '4rem'}} className="d-inline-block align-top" /></Link>
-     {' '}
-      Awesome Store
-      
+    <img src={Logo} alt="logo" style={{ height: '2.5rem'}} className="d-inline-block align-top" />
+    </Link>
      </Navbar.Brand>
     <Nav className="me-auto">
   
     </Nav>
     <Nav className="justify-content-end">
-    <Nav.Link ><Link to="/Login">Login</Link></Nav.Link>
-    <Nav.Link > 
+    <Nav.Link><Link to="/Login" style={{color: 'white'}}>Login</Link></Nav.Link>
+    <Nav.Link> 
     <Link to="/Cart">
-    <Button variant="warning">
-  Cart <Badge bg="secondary">9</Badge>
-</Button>
-</Link>
+    
+    <Badge bg="warning">
+    <img src={Cart} alt="Cart" style={{ height: '2rem'}} className="d-inline-block align-top" />
+    9
+    </Badge>
+    </Link>
+
 </Nav.Link>
     </Nav>
     </Container>
