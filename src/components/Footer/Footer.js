1c16c191b6ddfab5177e8a9fd63b219fe0cf31c3
Joe Kupernik <joekupernik@gmail.com>
2022-05-18 13:34:20 -0700

    footer css

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -13,11 +13,11 @@ function Footer() {
     return (
         <>
             <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top" style={{backgroundColor: 	'#212529'}}>
-                <div class="col-md-4 d-flex align-items-center">
+                <div class="col-md-4 d-flex align-items-center" style={{marginLeft: '2rem'}}>
                 <Link to="/"><Button variant="secondary">Home</Button></Link>
                     
                 </div>
-                <div className="text-center">
+                <div className="text-center" style={{marginRight: '2rem'}}>
                   <span class="text-muted">© 2021 Company, Inc</span>
                 </div>
             </footer>
